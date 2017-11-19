0f8ef5719fe16d64c09d0598aa80b3acd1f0b56d
Yanyi Liang <yanyeepanda@gmail.com>
2017-11-19 18:20:00 +1100

    inject service and call it

--- code-test/src/app/app.component.ts
+++ code-test/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { MockService } from './mock-service/mock-service.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'REA Group';
+  resultItems:any;
+  savedItems:any;
+  private loading: boolean = true;
+
+  constructor(public mockService:MockService){
+  }
+
+  ngOnInit() {
+    this.loading = true;
+    this.fetchItems();
+  };
+
+  fetchItems = () => {
+    this.loading = false;
+    this.resultItems = this.mockService.fetchMockDataItems().results;
+    this.savedItems = this.mockService.fetchMockDataItems().saved;
+
+    console.log(this.resultItems, this.savedItems);
+    
+  }
+  
 }
