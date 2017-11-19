73a632cac2ddf80663a57685ebd2ae7acc0af083
Yanyi Liang <yanyeepanda@gmail.com>
2017-11-19 22:49:00 +1100

    add move item function

--- code-test/src/app/app.component.ts
+++ code-test/src/app/app.component.ts
@@ -12,12 +12,14 @@ export class AppComponent {
   resultItems:any;
   savedItems:any;
   private loading: boolean = true;
+  private moving: boolean = false;
 
   constructor(public mockService:MockService){
   }
 
   ngOnInit() {
     this.loading = true;
+    this.moving = false;
     this.fetchItems();
   };
 
@@ -27,7 +29,26 @@ export class AppComponent {
     this.savedItems = this.mockService.fetchMockDataItems().saved;
 
     console.log(this.resultItems, this.savedItems);
+  }
+
+  moveItem = (itemId, fromList, toList) =>{
+    var itemToMove;
+    var index;
+    for (var n in fromList){
+      if (fromList[n].id == itemId){
+        itemToMove = fromList[n];
+        index = n;
+      }
+      break;
+    }
+
+    console.log(itemToMove, index);
+  
     
+    toList.push(itemToMove);
+    fromList.splice(index,1);
+
+    console.log(this.resultItems, this.savedItems);   
   }
   
 }
